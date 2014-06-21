a29f798e483273d881a3b5b754475e160254cbdb
Michael Harrington <kibiz0r@gmail.com>
2014-06-21 00:13:48 -0400

    Better phrasing

--- spec/lib/extrapolate_spec.rb
+++ spec/lib/extrapolate_spec.rb
@@ -2,17 +2,17 @@ require "spec_helper"
 
 describe "extrapolate" do
   describe "directive :my_directive => :resulting_delta do |arg1, arg2|" do
-    it "returns an un-enacted delta set that, when pushed, will call my_directive" do
+    it "returns an un-enacted delta set which, when pushed, will call my_directive" do
     end
   end
 
   describe "def my_method(arg)" do
-    it "returns an un-enacted delta set that, when pushed, will call my_method" do
+    it "returns an un-enacted delta set which, when pushed, will call my_method" do
     end
   end
 
   describe "delta :my_delta do" do
-    it "returns an un-enacted delta set that, when pushed, will apply my_delta" do
+    it "returns an un-enacted delta set which, when pushed, will apply my_delta" do
     end
   end
 end
